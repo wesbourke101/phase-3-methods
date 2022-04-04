df92d14fd17364a130d2e69a176bcc2c1967890e
wesley <wesbourkeavl@gmail.com>
2022-04-04 13:52:52 -0400

    commit tree 079e0e84057ed9ae7770475da856bc9c98347f17

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end   
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end    
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(number)
+    if number.class != Integer 
+        return nil
+end 
+    return number / 2   
+end
+
+
+
+
+
+
+
